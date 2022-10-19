770a21655757904824e02b510d27f9c724c3f425
sgsk88 <sgsk88@nate.com>
2022-10-19 18:38:24 +0900

    rv adapter line update

--- app/src/main/java/com/myongsik/myongsikandroid/ui/adapter/HomeFoodViewHolder.kt
+++ app/src/main/java/com/myongsik/myongsikandroid/ui/adapter/HomeFoodViewHolder.kt
@@ -1,5 +1,10 @@
 package com.myongsik.myongsikandroid.ui.adapter
 
+import android.graphics.Color
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
 import androidx.recyclerview.widget.RecyclerView
 import com.myongsik.myongsikandroid.data.model.FoodResult
 import com.myongsik.myongsikandroid.data.model.TodayFoodResponse
@@ -19,28 +24,47 @@ class HomeFoodViewHolder(
 
         val date = "${dayDate}년 ${dayMonth}월 ${dayDay}일 $day"
 
+        var weekFood : String = ""
+        var weekBFood : String = ""
+        var dinner : String = ""
+
+        weekFood = "${weekFoodResult.lunchA[0]} ${weekFoodResult.lunchA[1]} ${weekFoodResult.lunchA[2]}\n" +
+                "${weekFoodResult.lunchA[3]} ${weekFoodResult.lunchA[4]} ${weekFoodResult.lunchA[5]}"
+
+        weekBFood = "${weekFoodResult.lunchB[0]} ${weekFoodResult.lunchB[1]} ${weekFoodResult.lunchB[2]}\n" +
+                "${weekFoodResult.lunchB[3]}  ${weekFoodResult.lunchB[4]}  ${weekFoodResult.lunchB[5]} "
+
+        dinner = "${weekFoodResult.dinner[0]} ${weekFoodResult.dinner[1]} ${weekFoodResult.dinner[2]}\n" +
+                "${weekFoodResult.dinner[3]} ${weekFoodResult.dinner[4]} ${weekFoodResult.dinner[5]} "
+
+        // 3. SpannableStringBuilder 타입으로 변환
+        val builderWeekFood = SpannableStringBuilder(weekFood)
+
+        // 4-1. index=0 에 해당하는 문자열(0)에 볼드체적용
+        val boldSpanWeekFood = ForegroundColorSpan(Color.parseColor("#274984"))
+        builderWeekFood.setSpan(boldSpanWeekFood, 0, weekFoodResult.lunchA[0].length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        // 3. SpannableStringBuilder 타입으로 변환
+        val builderWeekBFood = SpannableStringBuilder(weekBFood)
+
+        // 4-1. index=0 에 해당하는 문자열(0)에 볼드체적용
+        val boldSpanWeekBFood = ForegroundColorSpan(Color.parseColor("#274984"))
+        builderWeekBFood.setSpan(boldSpanWeekBFood, 0, weekFoodResult.lunchB[0].length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        // 3. SpannableStringBuilder 타입으로 변환
+        val builderDinnerFood = SpannableStringBuilder(dinner)
+
+        // 4-1. index=0 에 해당하는 문자열(0)에 볼드체적용
+        val boldSpanDinner = ForegroundColorSpan(Color.parseColor("#274984"))
+        builderDinnerFood.setSpan(boldSpanDinner, 0, weekFoodResult.dinner[0].length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
         itemView.apply{
             binding.weekFoodDayOfWeekTv.text = date
-            binding.weekFood1.text = "${weekFoodResult.lunchA[0]} ${weekFoodResult.lunchA[1]} ${weekFoodResult.lunchA[2]}"
-//            binding.weekFood2.text = "${weekFoodResult.lunchA[1]} "
-//            binding.weekFood3.text = "${weekFoodResult.lunchA[2]}"
-            binding.weekFood4.text = "${weekFoodResult.lunchA[3]} "
-            binding.weekFood5.text = "${weekFoodResult.lunchA[4]} "
-            binding.weekFood6.text = "${weekFoodResult.lunchA[5]}"
+            binding.weekFood.text = builderWeekFood
 
-            binding.weekAfternoon2Food1.text = "${weekFoodResult.lunchB[0]} "
-            binding.weekAfternoon2Food2.text = "${weekFoodResult.lunchB[1]} "
-            binding.weekAfternoon2Food3.text = "${weekFoodResult.lunchB[2]}"
-            binding.weekAfternoon2Food4.text = "${weekFoodResult.lunchB[3]} "
-            binding.weekAfternoon2Food5.text = "${weekFoodResult.lunchB[4]} "
-            binding.weekAfternoon2Food6.text = "${weekFoodResult.lunchB[5]}"
+            binding.weekAfternoon2Food1.text = builderWeekBFood
 
-            binding.weekEveningFood1.text = "${weekFoodResult.dinner[0]} "
-            binding.weekEveningFood2.text = "${weekFoodResult.dinner[1]} "
-            binding.weekEveningFood3.text = "${weekFoodResult.dinner[2]}"
-            binding.weekEveningFood4.text = "${weekFoodResult.dinner[3]} "
-            binding.weekEveningFood5.text = "${weekFoodResult.dinner[4]} "
-            binding.weekEveningFood6.text = "${weekFoodResult.dinner[5]}"
+            binding.weekEveningFood1.text = builderDinnerFood
         }
 
     }
