d67db7b8550585d411043c3c39c4f07c9bb39254
d00hye <dohye11220@gmail.com>
2023-02-26 17:20:56 +0900

    자연캠 api 연결

--- app/src/main/java/com/myongsik/myongsikandroid/ui/adapter/food/ViewPagerAreaAdapter.kt
+++ app/src/main/java/com/myongsik/myongsikandroid/ui/adapter/food/ViewPagerAreaAdapter.kt
@@ -5,59 +5,29 @@ import android.text.SpannableStringBuilder
 import android.text.Spanned
 import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.myongsik.myongsikandroid.data.model.food.WeekFoodResult
 import com.myongsik.myongsikandroid.databinding.ItemHomeTodayFoodBinding
 
 //뷰페이저 어댑터
 //주간 음식 조회에서 사용
 class ViewPagerAreaAdapter(
-    private var weekFoodResult: List<WeekFoodResult>,
+    private var weekFoodResult: List<String>,
 ) : RecyclerView.Adapter<ViewPagerAreaAdapter.Pager2ViewHolder>() {
 
     inner class Pager2ViewHolder(
         private val binding : ItemHomeTodayFoodBinding
     ) : RecyclerView.ViewHolder(binding.root){
-        fun bind(weekFoodResult: WeekFoodResult){
-            val dayDate = weekFoodResult.toDay.substring(0, 4)
-            val dayMonth = weekFoodResult.toDay.substring(5, 7)
-            val dayDay = weekFoodResult.toDay.substring(8, 10)
+        fun bind(weekFoodResult: String){
 
-            val date = "${dayDate}년 ${dayMonth}월 ${dayDay}일"
 
-//            when(weekFoodResult.toDay.substring(8, 10)){
-//                "월요일" -> binding.viewPager2.setCurrentItem(0, true)
-//                "화요일" -> binding.viewPager2.setCurrentItem(1, true)
-//                "수요일" -> binding.viewPager2.setCurrentItem(2, true)
-//                "목요일" -> binding.viewPager2.setCurrentItem(3, true)
-//                "금요일" -> binding.viewPager2.setCurrentItem(4, true)
-//
-//            }
-
-            val weekFood = "${weekFoodResult.lunchA[0]} ${weekFoodResult.lunchA[1]} ${weekFoodResult.lunchA[2]} " +
-                    "${weekFoodResult.lunchA[3]} ${weekFoodResult.lunchA[4]} ${weekFoodResult.lunchA[5]}"
-
-            val weekBFood = "${weekFoodResult.lunchB[0]} ${weekFoodResult.lunchB[1]} ${weekFoodResult.lunchB[2]} " +
-                    "${weekFoodResult.lunchB[3]}  ${weekFoodResult.lunchB[4]}  ${weekFoodResult.lunchB[5]} "
-
-            val dinner = "${weekFoodResult.dinner[0]} ${weekFoodResult.dinner[1]} ${weekFoodResult.dinner[2]} " +
-                    "${weekFoodResult.dinner[3]} ${weekFoodResult.dinner[4]} ${weekFoodResult.dinner[5]} "
-
-            val builderWeekFood = SpannableStringBuilder(weekFood)
-
-            val boldSpanWeekFood = ForegroundColorSpan(Color.parseColor("#274984"))
-            builderWeekFood.setSpan(boldSpanWeekFood, 0, weekFoodResult.lunchA[0].length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-
-            val builderWeekBFood = SpannableStringBuilder(weekBFood)
-
-            val boldSpanWeekBFood = ForegroundColorSpan(Color.parseColor("#274984"))
-            builderWeekBFood.setSpan(boldSpanWeekBFood, 0, weekFoodResult.lunchB[0].length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+            val dinner = "${weekFoodResult[0]} ${weekFoodResult[1]} ${weekFoodResult[2]} " +
+                    "${weekFoodResult[3]} ${weekFoodResult[4]} ${weekFoodResult[5]} "
 
             val builderDinnerFood = SpannableStringBuilder(dinner)
-
             val boldSpanDinner = ForegroundColorSpan(Color.parseColor("#274984"))
-            builderDinnerFood.setSpan(boldSpanDinner, 0, weekFoodResult.dinner[0].length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+            builderDinnerFood.setSpan(boldSpanDinner, 0, weekFoodResult[0].toString().length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
 
             fun getGoodChange() {
                 binding.todayAfternoonGoodTv.setTextColor(Color.parseColor("#274984"))
@@ -81,30 +51,24 @@ class ViewPagerAreaAdapter(
             }
 
             itemView.apply{
-//                binding.weekFoodDayOfWeekTv.text = date
-                binding.todayFood1.text = builderWeekFood
-                binding.todayFoodLunchB.text = builderWeekBFood
+                // 석식만 하고 있음
+                binding.homeLunchAV.visibility = View.GONE
+                binding.homeLunchBV.visibility = View.GONE
+                binding.todayDayLunchA.visibility = View.GONE
+                binding.todayDayLunchB.visibility = View.GONE
+                binding.todayFood1.visibility = View.GONE
+                binding.todayFoodLunchB.visibility = View.GONE
+                binding.todayLineIv.visibility = View.GONE
+                binding.todayLunchBLineIv.visibility = View.GONE
+                binding.todayGoodCl.visibility = View.GONE
+                binding.todayGoodClLunchB.visibility = View.GONE
+                binding.todayHateCl.visibility = View.GONE
+                binding.todayHateClLunchB.visibility = View.GONE
+                binding.todayHateClLunchA.visibility = View.GONE
+                binding.todayHateClLunchB.visibility = View.GONE
+
+
                 binding.todayFoodAfternoon.text = builderDinnerFood
-
-//                binding.todayGoodCl.setOnClickListener{
-//                    if(Constant.LUNCH_A_GOOD == "good"){
-//                        Constant.LUNCH_A_GOOD = ""
-//                        mainViewModel.saveLunchEvaluation(weekFoodResult, "")
-//                    }else{
-//                        Constant.LUNCH_A_GOOD = "good"
-//                        mainViewModel.saveLunchEvaluation(weekFoodResult, "good")
-//                    }
-//                }
-//                binding.todayGoodClLunchB.setOnClickListener{
-//
-//                }
-//                binding.todayGoodClAfternoon.setOnClickListener{
-//
-//                }
-
-
-
-
 
             }
         }
