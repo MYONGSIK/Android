6b0ed1e269a779830c51220a5697426fedd0a386
KwakEuiJin <yuijin0909@gmail.com>
2023-03-16 00:21:45 +0900

    fix(viewHolder):ViewHolder 정리

--- app/src/main/java/com/myongsik/myongsikandroid/ui/adapter/search/SearchFoodViewHolder.kt
+++ app/src/main/java/com/myongsik/myongsikandroid/ui/adapter/search/SearchFoodViewHolder.kt
@@ -16,10 +16,8 @@ class SearchFoodViewHolder(
     private val binding: ItemRestaurantFoodBinding,
     private val mCallback: OnLoveClick,
 ) : RecyclerView.ViewHolder(binding.root) {
-
-
     fun bind(restaurant: Restaurant) {
-
+        val context = binding.root.context
         val distance = if (restaurant.distance.length >= 4) {
             "${restaurant.distance[0]}.${restaurant.distance[1]}km"
         } else {
@@ -33,77 +31,59 @@ class SearchFoodViewHolder(
                 restaurant.place_name
 
 
+        with(binding) {
+            // 찜콩 리스트
+            itemFoodLoveIv.visibility = View.INVISIBLE
+            itemFoodLoveFillIv.visibility = View.INVISIBLE
 
-        itemView.apply {
-            with(binding) {
-                // 찜콩 리스트
+            itemFoodLoveIv.setOnClickListener {
+                mCallback.addItem(restaurant)
                 itemFoodLoveIv.visibility = View.INVISIBLE
+                itemFoodLoveFillIv.visibility = View.VISIBLE
+                Snackbar.make(it, "찜 완료!", Snackbar.LENGTH_SHORT).show()
+            }
+            itemFoodLoveFillIv.setOnClickListener {
+                mCallback.deleteItem(restaurant)
+                itemFoodLoveIv.visibility = View.VISIBLE
                 itemFoodLoveFillIv.visibility = View.INVISIBLE
-
-                itemFoodLoveIv.setOnClickListener {
-                    mCallback.addItem(restaurant)
-                    itemFoodLoveIv.visibility = View.INVISIBLE
-                    itemFoodLoveFillIv.visibility = View.VISIBLE
-                    Snackbar.make(it, "찜 완료!", Snackbar.LENGTH_SHORT).show()
+                Snackbar.make(it, "찜 목록에서 삭제되었습니다.", Snackbar.LENGTH_SHORT).show()
+            }
+            itemFoodPhoneTv.setOnClickListener {
+                val text = itemFoodPhoneTv.text
+                if (context.getString(R.string.is_null_phone_number) != text) {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse("tel:$text"))
+                    context.startActivity(intent)
                 }
-                itemFoodLoveFillIv.setOnClickListener {
-                    mCallback.deleteItem(restaurant)
-                    itemFoodLoveIv.visibility = View.VISIBLE
-                    itemFoodLoveFillIv.visibility = View.INVISIBLE
-                    Snackbar.make(it, "찜 목록에서 삭제되었습니다.", Snackbar.LENGTH_SHORT).show()
-                }
-                itemFoodPhoneTv.setOnClickListener {
-                    val text = itemFoodPhoneTv.text
-                    if (context.getString(R.string.is_null_phone_number) != text) {
-                        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("tel:$text"))
-                        context.startActivity(intent)
-                    }
-                }
-
-                itemFoodLocationTv.setOnClickListener {
-                    val naverMapUrl = "nmap://search?query=${itemFoodLocationTv.text}"
-                    val uri = Uri.parse(naverMapUrl)
-                    val mapIntent = Intent(Intent.ACTION_VIEW, uri)
-                    mapIntent.setPackage("com.nhn.android.nmap")
-                    if (mapIntent.resolveActivity(context.packageManager) != null) {
-                        context.startActivity(mapIntent)
-                    } else {
-                        context.run {
-                            Toast.makeText(this, getString(R.string.please_install_naver_map), Toast.LENGTH_SHORT).show()
-                        }
-                    }
-                }
-
-
-
-                itemFoodName.text = placeName
-                itemFoodObject.text = restaurant.category_group_name
-                weekFoodAfternoonTv.text = distance
-                itemFoodLocationTv.text = restaurant.road_address_name
-
-                itemFoodPhoneTv.apply {
-                    text = if (restaurant.phone == "") {
-                        this.context.getString(R.string.is_null_phone_number)
-                    } else {
-                        restaurant.phone
-                    }
-                }
-
             }
 
+            itemFoodLocationTv.setOnClickListener {
+                val naverMapUrl = "nmap://search?query=${itemFoodLocationTv.text}"
+                val uri = Uri.parse(naverMapUrl)
+                val mapIntent = Intent(Intent.ACTION_VIEW, uri)
+                mapIntent.setPackage("com.nhn.android.nmap")
+                if (mapIntent.resolveActivity(context.packageManager) != null) {
+                    context.startActivity(mapIntent)
+                } else {
+                    context.run {
+                        Toast.makeText(this, getString(R.string.please_install_naver_map), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+            itemFoodName.text = placeName
+            itemFoodObject.text = restaurant.category_group_name
+            weekFoodAfternoonTv.text = distance
+            itemFoodLocationTv.text = restaurant.road_address_name
+
+            itemFoodPhoneTv.apply {
+                text = if (restaurant.phone == "") {
+                    this.context.getString(R.string.is_null_phone_number)
+                } else {
+                    restaurant.phone
+                }
+            }
         }
     }
 
 
 }
-/*
-"address_name":"서울 서대문구 홍제동 334-107",
-"category_group_code":"CE7",
-"category_group_name":"카페",
-"category_name":"음식점 \u003e 카페 \u003e 커피전문점",
-"distance":"1380","id":"956694628",
-"phone":"010-4021-5293",
-"place_name":"런어웨이",
-"place_url":"http://place.map.kakao.com/956694628",
-"road_address_name":"서울 서대문구 홍제내4길 4-11",
- */
