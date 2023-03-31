dab1d30e6bb9667c8e245bacabbd64eea4d59759
KwakEuiJin <yuijin0909@gmail.com>
2023-03-31 17:01:03 +0900

    feat(sort): dataStore key 추가

---
+++ app/src/main/java/com/myongsik/myongsikandroid/util/DataStoreKey.kt
@@ -0,0 +1,9 @@
+package com.myongsik.myongsikandroid.util
+
+import androidx.datastore.preferences.core.stringPreferencesKey
+
+object DataStoreKey {
+
+    val SORT_TYPE = stringPreferencesKey("sort_type")
+
+}
