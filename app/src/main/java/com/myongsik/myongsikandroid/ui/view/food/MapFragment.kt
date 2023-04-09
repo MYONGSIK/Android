2e3ce7346f254c74191c59448f0ffd5e2bc625df
SsongSik <sgsk88@nate.com>
2023-04-09 20:59:55 +0900

    [feat] marker click했을 때 storeId 추가

--- app/src/main/java/com/myongsik/myongsikandroid/ui/view/food/MapFragment.kt
+++ app/src/main/java/com/myongsik/myongsikandroid/ui/view/food/MapFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.myongsik.myongsikandroid.BaseFragment
 import com.myongsik.myongsikandroid.R
@@ -19,7 +20,7 @@ import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
 
 @AndroidEntryPoint
-class MapFragment : BaseFragment<FragmentMapBinding>() {
+class MapFragment : BaseFragment<FragmentMapBinding>(), MapView.POIItemEventListener {
 
     private val homeViewModel by viewModels<HomeViewModel>()
 
@@ -57,7 +58,7 @@ class MapFragment : BaseFragment<FragmentMapBinding>() {
 
                 val marker = MapPOIItem()
                 // 클릭했을 때 해당 가게 이름
-                marker.itemName = item.name
+                marker.itemName = item.storeId.toString()
                 marker.mapPoint = MapPoint.mapPointWithGeoCoord(latitude!!.toDouble(), longitude!!.toDouble())
                 // 현재 기본 마커 사용 추 후에 커스텀해서 바꿔야함
                 val customBitmap = createCustomMarkerBitmap(item.scrapCount.toString())
@@ -66,6 +67,7 @@ class MapFragment : BaseFragment<FragmentMapBinding>() {
                 marker.setCustomImageAnchor(0.5f, 1.0f)
 
                 mapView.addPOIItem(marker) // 지도에 마커 추가
+                mapView.setPOIItemEventListener(this)
             }
         }
     }
@@ -84,5 +86,26 @@ class MapFragment : BaseFragment<FragmentMapBinding>() {
         customMarkerView.draw(canvas)
 
         return bitmap
+    }
+
+    override fun onPOIItemSelected(p0: MapView?, p1: MapPOIItem?) {
+        Toast.makeText(requireContext(), "마커 클릭: ${p1?.itemName}", Toast.LENGTH_SHORT).show()
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onCalloutBalloonOfPOIItemTouched(p0: MapView?, p1: MapPOIItem?) {
+
+    }
+
+    override fun onCalloutBalloonOfPOIItemTouched(
+        p0: MapView?,
+        p1: MapPOIItem?,
+        p2: MapPOIItem.CalloutBalloonButtonType?
+    ) {
+
+    }
+
+    override fun onDraggablePOIItemMoved(p0: MapView?, p1: MapPOIItem?, p2: MapPoint?) {
+
     }
 }
