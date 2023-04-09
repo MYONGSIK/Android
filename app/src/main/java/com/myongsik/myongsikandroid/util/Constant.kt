af9e88d541bfb129b9bef2e42e6d2652675ca183
SsongSik <sgsk88@nate.com>
2023-04-09 15:55:30 +0900

    [fix] : 일부 Constant화

--- app/src/main/java/com/myongsik/myongsikandroid/util/Constant.kt
+++ app/src/main/java/com/myongsik/myongsikandroid/util/Constant.kt
@@ -12,6 +12,16 @@ object Constant {
     const val SEOUL = "SEOUL"
     const val YONGIN = "YONGIN"
 
+    const val SEOUL_CAMPUS = "서울 명지대"
+    const val YONGIN_CAMPUS = "용인 명지대"
+    const val CATEGORY_GROUP_CODE = "FD6, CE7"
+
+    const val SEOUL_CAMPUS_X = 126.923460283882
+    const val SEOUL_CAMPUS_Y = 37.5803504797164
+
+    const val YONGIN_CAMPUS_X = 127.18758354347
+    const val YONGIN_CAMPUS_Y = 37.224650469991
+
     const val DISTANCE = "distance"
     const val SCRAP_COUNT = "scrapCount"
     const val DESC = "desc"
