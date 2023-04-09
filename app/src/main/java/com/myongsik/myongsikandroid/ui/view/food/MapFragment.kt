b8b94ab959339add7c3aa0e4274eb24115ec97a7
SsongSik <sgsk88@nate.com>
2023-04-09 19:21:38 +0900

    [feat] : Map Campus 분리

--- app/src/main/java/com/myongsik/myongsikandroid/ui/view/food/MapFragment.kt
+++ app/src/main/java/com/myongsik/myongsikandroid/ui/view/food/MapFragment.kt
@@ -3,7 +3,10 @@ package com.myongsik.myongsikandroid.ui.view.food
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.myongsik.myongsikandroid.BaseFragment
+import com.myongsik.myongsikandroid.R
 import com.myongsik.myongsikandroid.databinding.FragmentMapBinding
+import com.myongsik.myongsikandroid.util.Constant
+import com.myongsik.myongsikandroid.util.MyongsikApplication
 import dagger.hilt.android.AndroidEntryPoint
 import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
@@ -28,7 +31,11 @@ class MapFragment : BaseFragment<FragmentMapBinding>() {
 
     private fun createMapView(){
         val mapView = MapView(requireActivity())
-        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.5803504797164, 126.923460283882), true)
+        if (MyongsikApplication.prefs.getUserCampus() == "Y") {
+            mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(Constant.YONGIN_CAMPUS_Y, Constant.YONGIN_CAMPUS_X), true)
+        } else{
+            mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(Constant.SEOUL_CAMPUS_Y, Constant.SEOUL_CAMPUS_X), true)
+        }
         val mapViewContainer = binding.mapView
         mapViewContainer.addView(mapView)
     }
