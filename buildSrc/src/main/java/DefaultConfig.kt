cd84aba67875452b1bd25e3e626ee306d6d09189
SsongSik <sgsk88@nate.com>
2023-08-24 00:52:32 +0900

    [fix] version code, name 수정

--- buildSrc/src/main/java/DefaultConfig.kt
+++ buildSrc/src/main/java/DefaultConfig.kt
@@ -2,6 +2,6 @@ object DefaultConfig {
     const val COMPILE_SDK_VERSION = 33
     const val MIN_SDK_VERSION = 23
     const val TARGET_SDK_VERSION = 33
-    const val VERSION_CODE = 26
-    const val VERSION_NAME = "2.2.11"
+    const val VERSION_CODE = 27
+    const val VERSION_NAME = "2.2.12"
 }
