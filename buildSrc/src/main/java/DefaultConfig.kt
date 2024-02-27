e657d96c54891ee4911f118065d5b09253aebbbd
SsongSik <sgsk88@nate.com>
2024-02-27 22:00:22 +0900

    version name, code up

--- buildSrc/src/main/java/DefaultConfig.kt
+++ buildSrc/src/main/java/DefaultConfig.kt
@@ -2,6 +2,6 @@ object DefaultConfig {
     const val COMPILE_SDK_VERSION = 34
     const val MIN_SDK_VERSION = 26
     const val TARGET_SDK_VERSION = 34
-    const val VERSION_CODE = 28
-    const val VERSION_NAME = "2.2.13"
+    const val VERSION_CODE = 29
+    const val VERSION_NAME = "2.2.14"
 }
